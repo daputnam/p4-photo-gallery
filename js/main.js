440d7e7d2f54aebe7f3a5f6342881178a4ffae17
Dan Putnam <daniel.a.putnam@gmail.com>
2016-03-19 17:47:12 -0500

    added lightbox, arrow not positioned properly

--- js/main.js
+++ js/main.js
@@ -36,6 +36,45 @@ $('.sticker').sticky({
   responsiveWidth: true
 });
 
+//lightbox code
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+var $caption = $("<p></p>");
+var $leftArrow = $("<div><p id='leftArrow'><</p></div>");
+var $rightArrow = $("<div><p id='rightArrow'>></p></div>");
+
+$overlay.append($leftArrow);
+$overlay.append($image);
+$overlay.append($rightArrow);
+// add overlay
+$overlay.append($caption);
+    //a caption
+$("body").append($overlay);
+// capture click on link to image
+$('#imageGallery a').click(function(event){
+  event.preventDefault();
+  console.log($(this).attr('href'));
+  //update overlay with the image linked in the link
+  //add caption from alt and title from title
+  var imageLocation = $(this).attr("href");
+  $image.attr("src", imageLocation);
+  //show overlay
+  $overlay.show();
+
+  //get alt tag and set as caption
+  var captionText = $(this).children("img").attr("alt");
+
+  $caption.text(captionText);
+
+});
+// when overlay is clicked 
+$('#overlay').click(function(){
+  //hide overlay
+  $overlay.hide();
+
+});
+  
+
 
 
 
